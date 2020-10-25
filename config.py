a9b84fc79358c9b098f4737fb32ef4e985e2e13d
Jan Mrlth <marloth@outlook.com>
2020-10-25 01:29:56 +0000

    first hardware tests successful

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    influxdb_host = os.getenv("InfluxdbHost")
-    influxdb_username = os.getenv("InfluxdbUser")
-    influxdb_password = os.getenv("InfluxdbPassword")
+    influxdb_host = os.getenv("influxdb_host")
+    influxdb_username = os.getenv("influxdb_username")
+    influxdb_password = os.getenv("influxdb_password")
     sensor_pin = 3
--- open-oee.py
+++ open-oee.py
@@ -8,23 +8,24 @@ from influxdb import InfluxDBClient
 import RPi.GPIO as Gpio
 
 
-
+print("Connecting to {}".format(Config.influxdb_host))
+IDB = InfluxDBClient(host=Config.influxdb_host, port=8086, username=Config.influxdb_username, password=Config.influxdb_password)
 
 def init():
     # Connect to Database
-    idb = InfluxDBClient(host=Config.influxdb_host, port=8086, username=Config.influxdb_username, password=Config.influxdb_password)
-    
+
 
     #Configue GPIO Pins
     Gpio.setmode(Gpio.BCM)
-    Gpio.setup(Config.sensor_pin, Gpio.IN)
-    Gpio.add_event_detect(Config.sensor_pin, Gpio.RISING, callback=send_data_for_one_mask)
+    Gpio.setup(3, Gpio.IN)
+    Gpio.add_event_detect(3, Gpio.FALLING, callback=send_data_for_one_mask, bouncetime=50)
     print("init abgeschlossen")
 
 
 def send_data_for_one_mask(channel):
     print("Send: one Mask Produced")
-    idb.write_points("mask, location=MedFacilities, machine=Nucleus, id=id output=1", time_precision='ms', database='MedOEE', protocol='line')
+    data = "mask,location=MedFacilities,machine=Nucleus,id=id output=1"
+    IDB.write_points(data, time_precision='ms', database='MedOEE', protocol='line')
 
 
 def main():
@@ -33,4 +34,5 @@ def main():
 
 if __name__ == "__main__": 
     init()
-    main()
+    while(True):
+        main()
