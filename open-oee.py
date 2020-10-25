9de085796292ecf94a9cc1735ec00fdd39868700
Jan Mrlth <marloth@outlook.com>
2020-10-25 22:40:25 +0000

    push

--- open-oee.py
+++ open-oee.py
@@ -1,6 +1,6 @@
 ## marloth_tech 2020 ##
 # Easy Tracker to measure machine output. 
-
+from os import popen
 from time import sleep
 from config import Config
 from influxdb import InfluxDBClient
@@ -11,23 +11,32 @@ import RPi.GPIO as Gpio
 print("Connecting to {}".format(Config.influxdb_host))
 IDB = InfluxDBClient(host=Config.influxdb_host, port=8086, username=Config.influxdb_username, password=Config.influxdb_password)
 
+
 def init():
     #Configue GPIO Pins
     Gpio.setmode(Gpio.BCM)
     Gpio.setup(3, Gpio.IN) #has internal Pull-Up 
-    Gpio.add_event_detect(3, Gpio.FALLING, callback=send_data_for_one_mask, bouncetime=50) 
+    Gpio.add_event_detect(3, Gpio.FALLING, callback=send_data_for_one_mask, bouncetime=150) 
     print("init complete")
 
 
 def send_data_for_one_mask(channel):
     print("Send: one Mask Produced")
-    data = "mask,location=MedFacilities,machine=Nucleus,id=id output=1"
-    IDB.write_points(data, time_precision='ms', database='MedOEE', protocol='line')
+    mask_data = "mask,location=MedFacilities,machine=Nucleus,id=id output=1"
+    IDB.write_points(mask_data, time_precision='ms', database='MedOEE', protocol='line')
+
+
+def send_core_temp():
+    temp = popen("vcgencmd measure_temp").readline().replace("temp=","").replace("'C","")
+    temp_data = "RPi,location=MedFacilities,machine=Nucleus,id=id temp=" + temp
+    IDB.write_points(temp_data, database='MedOEE', protocol='line')
 
 
 def main():
-    sleep(60)
-    print("main ended")
+    send_core_temp()
+    sleep(10)
+    print("10s")
+
 
 if __name__ == "__main__": 
     init()
