2b47cfae74e8c55e21eab76627aa2febc7b60b69
Jan Mrlth <marloth@outlook.com>
2020-11-01 00:13:43 +0100

    chnaged code to run on RevPi with DIO-Card

--- open-oee.py
+++ open-oee.py
@@ -1,42 +1,34 @@
 ## marloth_tech 2020 ##
 # Easy Tracker to measure machine output. 
-from os import popen
 from time import sleep
-from config import Config
+from accounts import Config
 from influxdb import InfluxDBClient
 #Raspberry GPIO
-import RPi.GPIO as Gpio
-
+import revpimodio2
 
 print("Connecting to {}".format(Config.influxdb_host))
 IDB = InfluxDBClient(host=Config.influxdb_host, port=8086, username=Config.influxdb_username, password=Config.influxdb_password)
 
+rpi = revpimodio2.RevPiModIO(autorefresh=True)
 
 def init():
-    #Configue GPIO Pins
-    Gpio.setmode(Gpio.BCM)
-    Gpio.setup(3, Gpio.IN) #has internal Pull-Up 
-    Gpio.add_event_detect(3, Gpio.FALLING, callback=send_data_for_one_mask, bouncetime=150) 
+    # Longer Cycletime due to 'long' request
+    rpi.cycletime = 50
+    # DIO Position in piCtory
+    dio = 32
+
+    # Register Event for I_2 
+    rpi.io.I_2.reg_event(send_data_for_one_mask, edge=revpimodio2.RISING)
     print("init complete")
 
-
-def send_data_for_one_mask(channel):
+def send_data_for_one_mask(ioname, iovalue):
     print("Send: one Mask Produced")
     mask_data = "mask,location=MedFacilities,machine=Nucleus,id=id output=1"
     IDB.write_points(mask_data, time_precision='ms', database='MedOEE', protocol='line')
 
 
-def send_core_temp():
-    temp = popen("vcgencmd measure_temp").readline().replace("temp=","").replace("'C","")
-    temp_data = "RPi,location=MedFacilities,machine=Nucleus,id=id temp=" + temp
-    IDB.write_points(temp_data, database='MedOEE', protocol='line')
-
-
 def main():
-    send_core_temp()
-    sleep(10)
-    print("10s")
-
+    rpi.mainloop()
 
 if __name__ == "__main__": 
     init()
